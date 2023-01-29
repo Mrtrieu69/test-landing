dd16b7608bfd017c9627ae0e33805610dd82f9d9
Mrtrieu69 <trieutam2808@gmail.com>
2023-01-29 10:35:05 +0300

    add animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,7 @@ const closeReserve = document.querySelector(".modal .close");
 const reserve = document.querySelector(".hero-cta");
 const submitReserve = document.querySelector(".form-reserve .btn.primary");
 
-// Menu on mobile and tablet
-toggleMenuMobile.addEventListener("click", handleShowMenuMobile);
-closeMenu.addEventListener("click", handleCloseMenuMobile);
-layout.addEventListener("click", handleCloseMenuMobile);
-
+// Toggle menu on mobile and tablet
 function handleShowMenuMobile() {
     menuMobile.classList.add("active");
     layout.classList.add("active");
@@ -23,11 +19,11 @@ function handleCloseMenuMobile() {
     layout.classList.remove("active");
 }
 
-// Modal form for reserve a table
-reserve.addEventListener("click", handleShowModalReserve);
-layoutReserve.addEventListener("click", handleCloseModalReserve);
-closeReserve.addEventListener("click", handleCloseModalReserve);
+toggleMenuMobile.addEventListener("click", handleShowMenuMobile);
+closeMenu.addEventListener("click", handleCloseMenuMobile);
+layout.addEventListener("click", handleCloseMenuMobile);
 
+// Modal form for reservation
 function handleShowModalReserve() {
     modal.classList.add("active");
 }
@@ -36,9 +32,28 @@ function handleCloseModalReserve() {
     modal.classList.remove("active");
 }
 
-// ajax request
+reserve.addEventListener("click", handleShowModalReserve);
+layoutReserve.addEventListener("click", handleCloseModalReserve);
+closeReserve.addEventListener("click", handleCloseModalReserve);
+
+// Ajax request
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+async function postData(data) {
+    const result = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    return result.json();
+}
+
 submitReserve.addEventListener("click", (e) => {
     e.preventDefault();
+
     const nameBooker = document.querySelector(".form-reserve .form-input[name='name']");
     const emailBooker = document.querySelector(".form-reserve .form-input[name='email']");
     const requestBooker = document.querySelector(
@@ -50,28 +65,26 @@ submitReserve.addEventListener("click", (e) => {
         return;
     }
 
-    const postObj = {
+    const data = {
         name: nameBooker.value,
         email: emailBooker.value,
         request: requestBooker.value,
     };
-    const post = JSON.stringify(postObj);
-    const url = "https://jsonplaceholder.typicode.com/posts";
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    xhr.send(post);
-    xhr.onload = function () {
-        if (xhr.status === 201) {
-            alert("A reservation successfully created!");
-        }
-    };
-
-    modal.classList.remove("active");
-    nameBooker.value = "";
-    emailBooker.value = "";
-    requestBooker.value = "";
+    // Post request
+    postData(data)
+        .then((res) => {
+            modal.classList.remove("active");
+            nameBooker.value = "";
+            emailBooker.value = "";
+            requestBooker.value = "";
+            console.log(res);
+            alert("Booking Success!");
+        })
+        .catch((err) => {
+            console.log(err);
+            alert("Booking Failed!");
+        });
 });
 
 // Slider (swiper)
@@ -95,3 +108,23 @@ const viewSwiper = new Swiper(".view .swiper", {
 });
 
 viewSwiper.slideTo(1, 0);
+
+// Animate on scroll
+function showAnimate() {
+    const animateItems = document.querySelectorAll("[data-animate]:not(.show)");
+
+    // Return when all animate items are showing
+    if (!animateItems.length) return;
+
+    animateItems.forEach((item) => {
+        const { innerHeight } = window;
+        const { top } = item.getBoundingClientRect();
+        if (top < innerHeight) {
+            item.classList.add("show");
+        }
+    });
+}
+
+showAnimate();
+
+document.addEventListener("scroll", showAnimate);
